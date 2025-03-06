188ae62ee08183e6845904b2c8ea0cc8056baf3c
Sreelakshmicc <sreelakshmicth@gmail.com>
2025-03-06 15:31:03 +0530

    Add files via upload

---
+++ Manipulation.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Manipulation
+{
+public static void main(String args[])
+{
+String txt;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a string:");
+txt=sc.nextLine();
+System.out.println("\nConverting string to lowercase:");
+System.out.println(txt.toLowerCase());
+System.out.println("\nConverting string to uppercase:");
+System.out.println(txt.toUpperCase());
+System.out.println("\nLength of the string:");
+System.out.println(txt.length());
+System.out.println("\nReplace the  string :");
+txt=sc.nextLine();
+
+String txt1;
+System.out.println("\nEnter a second string:");
+txt1=sc.nextLine();
+System.out.println("\nConcatenate first and second strings:");
+System.out.println(txt.concat(txt1));
+
+}
+}
+
